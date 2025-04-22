e152c2c180f27027d858dd268c7a346c8991a392
Htet Aung Khant <chitkogyi19950@gmail.com>
2025-04-22 12:59:26 +0700

    updated Button component

--- components/common/Button.js
+++ components/common/Button.js
@@ -1,13 +1,42 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
-const Button = ({ name, onClick }) => {
+export const LinkButton = ({ name, href, outline = false }) => {
+	return (
+		<Link
+			href={href}
+			className={`group w-fit p-1 flex items-center gap-3 lg:gap-2 inter-medium rounded-[60px] shadow-md cursor-pointer transition 
+				${outline ? "bg-transparent text-white border-[1px]" : "bg-white text-black"}
+			`}
+		>
+			<span className="px-2 lg:py-1 text-sm md:text-base xl:text-lg">{name}</span>
+			{
+				outline ? (
+					<Image src="/icon/export-white.svg" className="w-auto h-7 lg:h-9 transition duration-300 group-hover:rotate-45" width={46} height={46} alt="icon" />
+				) : (
+					<Image src="/icon/export-black.svg" className="w-auto h-7 lg:h-9 transition duration-300 group-hover:rotate-45" width={46} height={46} alt="icon" />	
+				)
+			}
+		</Link>
+	);
+}
+
+const Button = ({ name, onClick, outline = false }) => {
 	return (
 		<button
-			className="flex items-center gap-2 bg-white text-black poppins-semibold px-4 py-2 rounded-lg shadow-md cursor-pointer hover:bg-gray-100 transition"
 			onClick={onClick} // Ensures correct event handling
+			className={`group w-fit p-1 flex items-center gap-3 lg:gap-2 inter-medium rounded-[60px] shadow-md cursor-pointer transition 
+				${outline ? "bg-transparent text-white border-[1px]" : "bg-white text-black"}
+			`}
 		>
-			<span className="text-lg">{name}</span>
-			<img src="/export.svg" className="w-auto h-8" alt="icon" />
+			<span className="px-2 lg:py-1 text-sm md:text-base xl:text-lg">{name}</span>
+			{
+				outline ? (
+					<Image src="/icon/export-white.svg" className="w-auto h-7 lg:h-9 transition duration-300 group-hover:rotate-45" width={46} height={46} alt="icon" />
+				) : (
+					<Image src="/icon/export-black.svg" className="w-auto h-7 lg:h-9 transition duration-300 group-hover:rotate-45" width={46} height={46} alt="icon" />	
+				)
+			}
 		</button>
 	);
 };
