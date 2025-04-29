c3fbb2dd8506705ac34c889406c44e9ecfc01994
Htet Aung Khant <chitkogyi19950@gmail.com>
2025-04-29 23:09:26 +0700

    added "description" and "footer" prop to ListCard, bigger image

--- components/common/ListCard.js
+++ components/common/ListCard.js
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import Card from "./Card";
 
-const ListCard = ({ title, list, image }) => {
+const ListCard = ({ title, description, list, footer, image }) => {
     return (
         <Card className="bg-[#94B992]">
             <div className="w-full inter-font text-[#212A63] flex flex-col gap-4">
                 {title && (
                     <h2 className="text-2xl lg:text-3xl font-medium">{title}</h2>
                 )}
+                {description && <p>{description}</p>}
                 <div className="flex gap-4 items-center flex-col md:flex-row">
                     {list && (
                         <ul className="max-md:w-full list-disc pl-4 max-md:order-2">
@@ -20,10 +21,11 @@ const ListCard = ({ title, list, image }) => {
                     )}
                     {image && (
                         <div className="grow md:min-w-44 h-full flex justify-center items-center">
-                            <Image src={image} width={312} height={312} className="w-44 h-44 object-contain max-md:order-1" alt="icon" />
+                            <Image src={image} width={312} height={312} className="w-64 h-64 object-contain max-md:order-1" alt="icon" />
                         </div>
                     )}
                 </div>
+                {footer && <p>{footer}</p>}
             </div>
         </Card>
     )
