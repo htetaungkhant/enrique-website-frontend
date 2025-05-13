4b0c89d9080f1eff76fa6b53ff356b3891bd8e0e
Htet Aung Khant <chitkogyi19950@gmail.com>
2025-05-13 13:53:10 +0700

    changed the break point of InfoSection from "lg" to "xl"

--- components/common/InfoSection.js
+++ components/common/InfoSection.js
@@ -5,20 +5,20 @@ import { cn } from "@/lib/utils";
 
 export const UniformInfoSection = ({ image, children, imageAspectRatio = "default", reverse = false, className }) => {
     const gridCols = { // 2xl:flex
-        default: reverse ? "lg:grid-cols-[70%_25%] 2xl:grid-cols-[82%_15%] 2xl:justify-between 2xl:gap-10" : "lg:grid-cols-[25%_70%] 2xl:grid-cols-[15%_82%] 2xl:justify-between 2xl:gap-10",
-        portrait: reverse ? "lg:grid-cols-[75%_20%] 2xl:grid-cols-[85%_12%] 2xl:justify-between 2xl:gap-10" : "lg:grid-cols-[20%_75%] 2xl:grid-cols-[12%_85%] 2xl:justify-between 2xl:gap-10",
-        landscape: reverse ? "lg:grid-cols-[65%_30%] 2xl:grid-cols-[77%_20%] 2xl:justify-between 2xl:gap-10" : "lg:grid-cols-[30%_65%] 2xl:grid-cols-[20%_77%] 2xl:justify-between 2xl:gap-10",
+        default: reverse ? "xl:grid-cols-[70%_25%] 2xl:grid-cols-[82%_15%] 2xl:justify-between 2xl:gap-10" : "xl:grid-cols-[25%_70%] 2xl:grid-cols-[15%_82%] 2xl:justify-between 2xl:gap-10",
+        portrait: reverse ? "xl:grid-cols-[75%_20%] 2xl:grid-cols-[85%_12%] 2xl:justify-between 2xl:gap-10" : "xl:grid-cols-[20%_75%] 2xl:grid-cols-[12%_85%] 2xl:justify-between 2xl:gap-10",
+        landscape: reverse ? "xl:grid-cols-[65%_30%] 2xl:grid-cols-[77%_20%] 2xl:justify-between 2xl:gap-10" : "xl:grid-cols-[30%_65%] 2xl:grid-cols-[20%_77%] 2xl:justify-between 2xl:gap-10",
     }
 
     return (
-        <UPSection className={cn(`${gridCols[imageAspectRatio]} max-lg:flex max-lg:flex-col max-lg:justify-center max-lg:items-center lg:grid gap-10 lg:gap-16`, className)}>
-            <div className={`order-2 ${reverse ? "lg:order-1" : "lg:order-2"} flex flex-col gap-6`}>
+        <UPSection className={cn(`${gridCols[imageAspectRatio]} max-xl:flex max-xl:flex-col max-xl:justify-center max-xl:items-center xl:grid gap-10 xl:gap-16`, className)}>
+            <div className={`order-2 ${reverse ? "xl:order-1" : "xl:order-2"} flex flex-col gap-6`}>
                 {children}
             </div>
             {
                 image && (
                     <div
-                        className={`min-h-80 max-sm:w-full sm:min-h-64 max-lg:w-1/2 lg:min-h-40 3xl:min-h-[26rem] order-1 ${reverse ? "lg:order-2" : "lg:order-1"} relative`} // 2xl:min-h-80 
+                        className={`min-h-80 max-sm:w-full sm:min-h-64 max-xl:w-1/2 xl:min-h-40 3xl:min-h-[26rem] order-1 ${reverse ? "xl:order-2" : "xl:order-1"} relative`} // 2xl:min-h-80 
                     >
                         <Image
                             src={image}
