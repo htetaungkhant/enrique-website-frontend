bb121fdba353d2b07d1f6cead072a94a1b450f82
Htet Aung Khant <chitkogyi19950@gmail.com>
2025-05-17 18:02:31 +0700

    added UserButton component

--- components/common/Button.js
+++ components/common/Button.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { FaArrowRight } from "react-icons/fa6";
 import { MdArrowOutward } from "react-icons/md";
+import { FaRegUserCircle } from "react-icons/fa";
 
 import { cn } from "@/lib/utils";
 
@@ -122,4 +123,100 @@ export const IconButton = ({
 	);
 };
 
-export default IconButton;
+export const UserButton = ({
+	title,
+	onClick,
+	href,
+	outline = true,
+	className,
+	textClassName,
+	iconClassName,
+	reverseIconPosition = false,
+	size = "md", // "sm", "md", "lg"
+	...props
+}) => {
+	const sizeStyles = {
+		sm: {
+			button: "p-1 gap-1 rounded-3xl",
+			text: "p-1 text-xs md:text-sm",
+			icon: "h-6 lg:h-8",
+			iconSize: 28,
+		},
+		md: {
+			button: "p-1 gap-3 lg:gap-2 rounded-[60px]",
+			text: "px-2 lg:py-1 text-sm md:text-base",
+			icon: "h-7 lg:h-9",
+			iconSize: 46,
+		},
+		lg: {
+			button: "p-2 gap-4 rounded-[80px]",
+			text: "px-2 lg:py-2 text-base md:text-lg",
+			icon: "h-8 lg:h-10",
+			iconSize: 60,
+		},
+	};
+
+	const s = sizeStyles[size] || sizeStyles.md;
+
+	const classes = cn(
+		"group h-fit flex items-center justify-between inter-medium shadow-md cursor-pointer transition",
+		reverseIconPosition && "flex-row-reverse",
+		outline ? "bg-transparent text-white border-[1px]" : "bg-white text-black",
+		s.button,
+		className
+	);
+
+	const content = (
+		<>
+			<FaRegUserCircle
+				size={s.iconSize}
+				className={cn(
+					`p-1 border-2 text-white rounded-full w-auto transition duration-300`,
+					outline ? "border-white bg-transparent" : "border-black bg-black",
+					s.icon,
+					iconClassName
+				)}
+			/>
+			<span className={cn("flex-1 text-center", s.text, textClassName)}>{title}</span>
+		</>
+	);
+
+	if (href && props.download) {
+		return (
+			<a href={href} className={classes} {...props}>
+				{content}
+			</a>
+		);
+	}
+
+	if (href) {
+		return (
+			<Link href={href} className={classes} {...props}>
+				{content}
+			</Link>
+		);
+	}
+
+	return (
+		<button onClick={onClick} className={classes} {...props}>
+			{content}
+		</button>
+	);
+};
+
+const Button = ({
+	className,
+	children,
+	...props
+}) => {
+	return (
+		<button
+			className={cn("p-2 rounded-full bg-[#212A63] text-white font-medium cursor-pointer hover:shadow-lg disabled:bg-[#212a63be] disabled:cursor-not-allowed", className)}
+			{...props}
+		>
+			{children}
+		</button>
+	)
+}
+
+export default Button;
