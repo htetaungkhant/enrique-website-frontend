dc587e3ce849caa3856f435c2709bef301aa8013
Htet Aung Khant <chitkogyi19950@gmail.com>
2025-05-17 19:23:13 +0700

    .

--- components/common/Checkbox.js
+++ components/common/Checkbox.js
@@ -20,7 +20,7 @@ const Checkbox = ({
                     onChange={onChange}
                     className="peer appearance-none w-5 h-5 border border-[#212A63] rounded-xs bg-white transition-colors"
                 />
-                <span className="pointer-events-none absolute left-0 top-0 w-5 h-5 flex items-center justify-center">
+                <span className="absolute left-0 top-0 w-5 h-5 flex items-center justify-center"> {/* pointer-events-none */}
                     {checked && (
                         <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M4.5 8.5L7 11L11.5 6.5" stroke="#212A63" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
