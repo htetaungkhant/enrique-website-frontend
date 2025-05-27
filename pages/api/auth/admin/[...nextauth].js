4757f49c25649df0d05071e3c5deb65b8a754710
Htet Aung Khant <chitkogyi19950@gmail.com>
2025-05-27 10:16:17 +0700

    renamed from controllers to auth-route

--- pages/api/auth/admin/[...nextauth].js
+++ pages/api/auth/admin/[...nextauth].js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-import { validateAdminCredentials } from "@/lib/inhouseAPI/controllers";
+import { validateAdminCredentials } from "@/lib/inhouseAPI/auth-route";
 
 export const adminAuthOptions = {
     providers: [
