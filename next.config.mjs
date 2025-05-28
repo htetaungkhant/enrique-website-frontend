d28fca50560e4a6b53267598ede7b73958e7d403
Htet Aung Khant <chitkogyi19950@gmail.com>
2025-05-28 12:54:24 +0700

    added s3 images.remotePatterns at next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 's3.eu-west-1.amazonaws.com',
+        port: '',
+        pathname: '/arise-api/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
