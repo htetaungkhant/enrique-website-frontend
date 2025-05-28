3798ab1e7a965eed83cc89b31b58cac439a4de01
Htet Aung Khant <chitkogyi19950@gmail.com>
2025-05-28 12:08:35 +0700

    updated scroll-area by adding "scrollBarThumb" prop

--- components/ui/scroll-area.jsx
+++ components/ui/scroll-area.jsx
@@ -5,6 +5,7 @@ import { cn } from "@/lib/utils"
 
 function ScrollArea({
   className,
+  scrollBarThumb,
   children,
   ...props
 }) {
@@ -15,7 +16,7 @@ function ScrollArea({
         className="focus-visible:ring-ring/50 size-full rounded-[inherit] transition-[color,box-shadow] outline-none focus-visible:ring-[3px] focus-visible:outline-1">
         {children}
       </ScrollAreaPrimitive.Viewport>
-      <ScrollBar />
+      <ScrollBar thumb={scrollBarThumb} />
       <ScrollAreaPrimitive.Corner />
     </ScrollAreaPrimitive.Root>
   );
@@ -24,6 +25,7 @@ function ScrollArea({
 function ScrollBar({
   className,
   orientation = "vertical",
+  thumb = true,
   ...props
 }) {
   return (
@@ -33,15 +35,19 @@ function ScrollBar({
       className={cn(
         "flex touch-none p-px transition-colors select-none",
         orientation === "vertical" &&
-          "h-full w-2.5 border-l border-l-transparent",
+        "h-full w-2.5 border-l border-l-transparent",
         orientation === "horizontal" &&
-          "h-2.5 flex-col border-t border-t-transparent",
+        "h-2.5 flex-col border-t border-t-transparent",
         className
       )}
       {...props}>
-      <ScrollAreaPrimitive.ScrollAreaThumb
-        data-slot="scroll-area-thumb"
-        className="bg-border relative flex-1 rounded-full" />
+      {
+        thumb && (
+          <ScrollAreaPrimitive.ScrollAreaThumb
+            data-slot="scroll-area-thumb"
+            className="bg-border relative flex-1 rounded-full" />
+        )
+      }
     </ScrollAreaPrimitive.ScrollAreaScrollbar>
   );
 }
