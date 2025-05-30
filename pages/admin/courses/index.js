95c335f26d60141896da9bae94c60cb41634f9cf
Htet Aung Khant <chitkogyi19950@gmail.com>
2025-05-30 21:46:34 +0700

    just remove the dummy data of courses page

--- pages/admin/courses/index.js
+++ pages/admin/courses/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
 import AdminPagesWrapper from "@/components/Admin/PageWrapper";
@@ -13,67 +14,6 @@ import {
     PaginationNext,
     PaginationPrevious,
 } from "@/components/ui/pagination";
-import { useEffect, useState } from "react";
-
-// dummy data for cards list
-// const dummyData = [
-//     {
-//         "id": "01971b30-80a3-7000-b89a-23bf35d749f0",
-//         "price": 120,
-//         "image": {
-//             "id": "01971b30-903f-7000-a54e-1e7f1a9c81b1",
-//             "image": "https://s3.eu-west-1.amazonaws.com/arise-api/course/01971b30-903f-7000-a54e-1e7f1a9c81b1_original.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA2HUZ2C4HNRMJMJ6V%2F20250529%2Feu-west-1%2Fs3%2Faws4_request&X-Amz-Date=20250529T111854Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=0843999aec5355be816a86bba2282b26347926154f8243b3c277a8710fced77e",
-//             "thumbnail": "https://s3.eu-west-1.amazonaws.com/arise-api/course/01971b30-903f-7000-a54e-1e7f1a9c81b1_thumbnail.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA2HUZ2C4HNRMJMJ6V%2F20250529%2Feu-west-1%2Fs3%2Faws4_request&X-Amz-Date=20250529T111854Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=65ae24d0240ca311d2a22373a8bae5a4407ef09e887c9f0b16a7c94c4fa76c8a"
-//         },
-//         "title": "test title",
-//         "createdBy": {
-//             "name": "test created by",
-//             "image": {
-//                 "id": "01971b30-80a4-7000-9ebf-31794956e70f",
-//                 "image": "https://s3.eu-west-1.amazonaws.com/arise-api/course/01971b30-80a4-7000-9ebf-31794956e70f_original.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA2HUZ2C4HNRMJMJ6V%2F20250529%2Feu-west-1%2Fs3%2Faws4_request&X-Amz-Date=20250529T111854Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=ada1ade585b25378a51c805e2b95646865beb5703878e5b27601fc8d757eda3b",
-//                 "thumbnail": "https://s3.eu-west-1.amazonaws.com/arise-api/course/01971b30-80a4-7000-9ebf-31794956e70f_thumbnail.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA2HUZ2C4HNRMJMJ6V%2F20250529%2Feu-west-1%2Fs3%2Faws4_request&X-Amz-Date=20250529T111854Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=5fcdaf826e184145bfa45f17fabe591ec9de5379ff08320c593439e6d2bb8d5a"
-//             }
-//         },
-//         "sessionOverview": "test sessionOverview",
-//         "extraDetails": [
-//             {
-//                 "title": "Course Title",
-//                 "points": [
-//                     "Point 1",
-//                     "Point 2"
-//                 ]
-//             },
-//             {
-//                 "title": "Course Title",
-//                 "points": [
-//                     "Point 1",
-//                     "Point 2"
-//                 ]
-//             }
-//         ],
-//         "courseVideos": [
-//             {
-//                 "video": "course/01961a28-c543-7000-a589-acc0e40b411e.MP4",
-//                 "title": "Introduction to the Course",
-//                 "points": [
-//                     "Welcome to the course",
-//                     "Overview of the course structure",
-//                     "What you will learn"
-//                 ]
-//             }, {
-//                 "video": "course/01961a28-c543-7000-a589-acc0e40b411e.MP4",
-//                 "title": "Understanding the Basics",
-//                 "points": [
-//                     "Introduction to the basics",
-//                     "Key concepts explained",
-//                     "Practical examples"
-//                 ]
-//             }
-//         ],
-//         "createdAt": "2025-05-29T08:37:37.649Z",
-//         "updatedAt": "2025-05-29T08:37:37.649Z"
-//     }
-// ]
 
 export async function getServerSideProps(context) {
     try {
