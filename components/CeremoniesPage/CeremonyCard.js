2a0a7aee6cd62e6a5c8326e853f86772773759c6
Htet Aung Khant <chitkogyi19950@gmail.com>
2025-06-03 11:24:15 +0700

    adjust user CeremonyCard design

--- components/CeremoniesPage/CeremonyCard.js
+++ components/CeremoniesPage/CeremonyCard.js
@@ -7,20 +7,20 @@ import { ReadMoreBtn } from "../common/Button";
 
 const CeremonyCard = ({ image, title, locations, dates, time, learnMoreHref, className }) => {
     return (
-        <div className={cn("inter-font text-[#403D4E] bg-white rounded-3xl shadow-xl ring ring-[#A5F3CC40] shadow-[#A5F3CC40] overflow-hidden", className)}>
+        <div className={cn("inter-font text-[#403D4E] bg-white rounded-3xl shadow-xl ring ring-[#A5F3CC40] shadow-[#A5F3CC40] flex flex-col overflow-hidden", className)}>
             <div className="h-48 rounded-3xl bg-center bg-cover bg-no-repeat relative" style={{ backgroundImage: `url(${image})` }}></div>
-            <div className="pt-10 -mt-5 rounded-bl-3xl border-l-8 border-[#3FA535] px-4 py-6 flex flex-col gap-3">
-                <h4 className="text-xl font-bold">{title}</h4>
-                <div className="font-medium flex flex-col gap-1">
-                    <div className="flex gap-3 items-center">
+            <div className="flex-1 pt-10 -mt-5 rounded-bl-3xl border-l-8 border-[#3FA535] px-4 py-6 flex flex-col gap-3">
+                <h4 className="text-lg lg:text-xl font-bold">{title}</h4>
+                <div className="flex-1 font-medium flex flex-col gap-1">
+                    <div className="flex gap-3 items-center text-xs md:text-sm">
                         <FaLocationDot className="text-[#1C3896] bg-[#1C389659] p-1 rounded-full w-5 h-5" />
                         <p>{locations}</p>
                     </div>
-                    <div className="flex gap-3 items-center">
+                    <div className="flex gap-3 items-center text-xs md:text-sm">
                         <IoCalendarClear className="text-[#1C3896] bg-[#1C389659] p-1 rounded-full w-5 h-5" />
                         <p>{dates}</p>
                     </div>
-                    <div className="flex gap-3 items-center">
+                    <div className="flex gap-3 items-center text-xs md:text-sm">
                         <MdOutlineAccessTimeFilled className="text-[#1C3896] bg-[#1C389659] p-1 rounded-full w-5 h-5" />
                         <p>{time}</p>
                     </div>
