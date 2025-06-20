4aa81c8b8981217096323bb701d7de8e4099b380
Htet Aung Khant <chitkogyi19950@gmail.com>
2025-06-20 21:25:25 +0700

    feat: add Contact Us management page with search and pagination functionality

--- components/ui/dialog.jsx
+++ components/ui/dialog.jsx
@@ -46,6 +46,7 @@ const DialogOverlay = React.forwardRef(function DialogOverlay(
 
 function DialogContent({
   className,
+  closeIconClassName,
   children,
   ...props
 }) {
@@ -61,7 +62,10 @@ function DialogContent({
         {...props}>
         {children}
         <DialogPrimitive.Close
-          className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4">
+          className={cn(
+            "ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+            closeIconClassName,
+          )}>
           <XIcon />
           <span className="sr-only">Close</span>
         </DialogPrimitive.Close>
