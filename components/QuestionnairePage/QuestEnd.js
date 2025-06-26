8abbdf3a078860c55ae51a12889cd68f70486270
Htet Aung Khant <chitkogyi19950@gmail.com>
2025-06-26 14:34:22 +0700

    feat: update YouTubeBanner link and enhance navigation buttons in QuestEnd component for improved user engagement

--- components/QuestionnairePage/QuestEnd.js
+++ components/QuestionnairePage/QuestEnd.js
@@ -8,9 +8,11 @@ const QuestEnd = ({
 
     return (
         <div className={cn("flex flex-col items-center lg:gap-7 gap-5 text-white merriweather-font font-medium", className)}>
-            <YouTubeBanner className="w-full md:w-9/10 xl:w-3/4" />
-            <div className="mt-10">
-                <IconButton title="Start your Journey" href="/safety" iconAnimate={false} />
+            <YouTubeBanner href="https://youtu.be/GcjK7eRr1u4?si=fP9UQDfusVPFfYFM" className="w-full md:w-9/10 xl:w-3/4" />
+            <div className="mt-10 flex gap-x-5">
+                <IconButton className="w-33" title="Home" href="/" reverseIconPosition={true} iconAnimate={false} />
+
+                <IconButton title="View More" href="/safety" iconAnimate={false} />
             </div>
         </div>
     )
