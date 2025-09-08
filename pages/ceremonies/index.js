674cfc06b44a1ffd28f0018ace2634c85d6e29f9
Htet Aung Khant <chitkogyi19950@gmail.com>
2025-09-08 13:05:27 +0700

    fix: Set default sorting order to ascending for ceremonies

--- pages/ceremonies/index.js
+++ pages/ceremonies/index.js
@@ -39,7 +39,7 @@ export async function getServerSideProps(context) {
       body: {
         page,
         limit: 10,
-        sortByDate,
+        sortByDate: sortByDate || "asc",
       },
     });
 
